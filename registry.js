845fa644928524953512d7f595a734eeb4079b18
Mario Pareja <pareja.mario@gmail.com>
2023-06-06 15:26:43 -0400

    registry: add support for configured private registries

--- registry.js
+++ registry.js
@@ -1,10 +1,42 @@
+import Config from '@npmcli/config'
 import pacote from 'pacote'
+import path from 'path'
+import { createRequire } from 'node:module'
 import { promises as fs } from 'fs'
 
+const require = createRequire(import.meta.url)
+
 export class Registry {
+  constructor () {
+    this.loadNpmConfigP = this.loadNpmConfig()
+  }
+
+  async loadNpmConfig () {
+    // much of this is tediously recreated from staring at `npm` source code
+    const npmPath = path.dirname(require.resolve('npm'))
+    const utilsPath = path.resolve(npmPath, 'lib', 'utils', 'config', 'index.js')
+    const { definitions, flatten, shorthands } = require(utilsPath)
+
+    const config = new Config({
+      npmPath,
+      definitions,
+      flatten,
+      shorthands,
+      argv: []
+    })
+    await config.load()
+
+    this.config = config.flat
+  }
+
   async getPackument (dependency) {
+    await this.loadNpmConfigP
+
     // fullMetadata is required in order to get publish timestamps
-    return pacote.packument(dependency, { fullMetadata: true })
+    return pacote.packument(dependency, {
+      ...this.config,
+      fullMetadata: true
+    })
   }
 }
 
