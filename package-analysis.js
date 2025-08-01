a7b2b9fb114d4f66c436553e484c01849a5aabba
Mario Pareja <pareja.mario@gmail.com>
2025-08-01 16:05:56 -0400

    PackageAnalysis: remove moved method

--- package-analysis.js
+++ package-analysis.js
@@ -1,4 +1,3 @@
-import { promises as fs } from 'fs'
 import pickManifest from 'npm-pick-manifest'
 
 export class PackageAnalysis {
@@ -7,28 +6,6 @@ export class PackageAnalysis {
   constructor (registry, options) {
     this.registry = registry
     this.options = options
-  }
-
-  async anaylzePackages (packageJsonPaths) {
-    for await (const packageJsonPath of packageJsonPaths) {
-      const packageLockJsonPath = packageJsonPath.replace(/package\.json$/, 'package-lock.json')
-      const [pkg, pkgLock] = await Promise.allSettled([
-        fs.readFile(packageJsonPath, 'utf8'),
-        fs.readFile(packageLockJsonPath, 'utf8')
-      ])
-
-      if (pkg.status === 'rejected') {
-        throw pkg.reason
-      }
-      const packageJson = JSON.parse(pkg.value)
-
-      let packageLockJson = null
-      if (pkgLock.status === 'fulfilled') {
-        packageLockJson = JSON.parse(pkgLock.value)
-      }
-
-      await this.analyze(packageJson, packageLockJson)
-    }
   }
 
   async analyze (packageJson, packageLockJson) {
